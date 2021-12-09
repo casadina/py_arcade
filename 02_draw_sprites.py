537c0db6aa28a7a5d0356f56ca6f14372ac68010
Casadina <casadina@tuta.io>
2021-12-09 22:27:54 +0800

    Start Arcade tutorial. Draw window. Start part two.

---
+++ 02_draw_sprites.py
@@ -0,0 +1,40 @@
+#! /usr/bin/env python3
+"""02_draw_sprites.py - Opening a window in Arcade"""
+import arcade
+
+# Constraints
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 650
+SCREEN_TITLE = "Platformer"
+
+
+class MyGame(arcade.Window):
+    """
+    Main application class.
+    """
+
+    def __init__(self):
+        """Call the parent class and set up the window."""
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+
+        arcade.set_background_color(arcade.csscolor.CORNFLOWER_BLUE)
+
+    def setup(self):
+        """Set-up the game here. Call this function to restart the game."""
+
+    def on_draw(self):
+        """Render the screen."""
+
+        arcade.start_render()
+        # Code to draw the screen goes here.
+
+
+def main():
+    """Main program code."""
+    window = MyGame()
+    window.setup()
+    arcade.run()
+
+
+if __name__ == "__main__":
+    main()
